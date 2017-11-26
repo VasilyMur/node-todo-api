395d09c852e43483947893270d274a78fa6bc79d
Vasily Muravyev <vmouravi@gmail.com>
2017-11-26 17:28:12 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -8,21 +8,19 @@ const {User} = require('./models/user');
 const app = express();
 app.use(bodyParser.json());
 
-
 app.post('/todos', (req, res) => {
+
   const todo = new Todo({
     text: req.body.text
   });
 
   todo.save().then((doc) => {
-    res.send(doc)
+    res.send(doc);
   }, (err) => {
     res.status(400).send(err);
   });
-});
 
-
-
+})
 
 
 
@@ -30,3 +28,8 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+
+module.exports = {
+  app: app
+}
