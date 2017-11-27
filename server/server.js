6155358d30aa61e490258e2b77e1ee3c203d8a46
Vasily Muravyev <vmouravi@gmail.com>
2017-11-27 13:57:13 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 const app = express();
 app.use(bodyParser.json());
@@ -32,6 +33,27 @@ app.get('/todos', (req, res) => {
     res.status(400).send(err);
   });
 });
+
+
+
+// Route 3: Individual todos
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todos) => {
+    if(!todos) {
+      return res.status(404).send();
+    }
+    res.send({todos});
+  }).catch((err) => {
+    return res.status(400).send();
+  })
+
+})
 
 
 
